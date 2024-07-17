ff3c5951cf3c2bb12f65c14d1547d75d416798e3
showgo-koinuma <van71413989i@gmail.com>
2024-07-17 13:46:39 +0900

    [add] 敵のAbilityとBehaviorTreeの追加

---
+++ Source/SEKIRO/Private/Character/EnemyCharacter.cpp
@@ -0,0 +1,18 @@
+#include "Character/EnemyCharacter.h"
+
+void AEnemyCharacter::GetActiveAbilitiesWithTags(const FGameplayTagContainer AbilityTags,
+                                                 TArray<UGameplayAbility*>& ActiveAbilities) const
+{
+	TArray<FGameplayAbilitySpec*> AbilitiesToActivate;
+	AbilitySystemComponent->GetActivatableGameplayAbilitySpecsByAllMatchingTags(AbilityTags, AbilitiesToActivate, false);
+
+	for (const auto Spec : AbilitiesToActivate)
+	{
+		TArray<UGameplayAbility*> AbilityInstances = Spec->GetAbilityInstances();
+
+		for (const auto ActiveAbility : AbilityInstances)
+		{
+			ActiveAbilities.Add(Cast<UGameplayAbility>(ActiveAbility));
+		}
+	}
+}
---
+++ Source/SEKIRO/Public/Character/EnemyCharacter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+UCLASS()
+class SEKIRO_API AEnemyCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	// 発動中のAbilityを取得
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	void GetActiveAbilitiesWithTags(const FGameplayTagContainer AbilityTags, TArray<UGameplayAbility*>& ActiveAbilities) const;
+};
